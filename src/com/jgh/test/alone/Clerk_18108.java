607af5195c392f5fb1e915dce5dacc42b5078234
Jogeahyun <whro815@naver.com>
2023-12-19 18:34:34 +0900

    서시, 나머지 문제

---
+++ src/com/jgh/test/alone/Clerk_18108.java
@@ -0,0 +1,26 @@
+package com.jgh.test.alone;
+
+import java.util.Scanner;
+
+public class Clerk_18108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        System.out.println(x-543);
+
+       /* int year = -1;
+        int chgYear = -1;
+
+        do{
+            System.out.println("서기 연도 1000 이상 3000이하로 입력해주세요!!");
+            System.out.print(">>");
+            year = sc.nextInt();
+        } while (!(1000 <= year && year <= 3000));
+
+        chgYear = year - 543;
+        System.out.println(chgYear);*/
+    }
+}
