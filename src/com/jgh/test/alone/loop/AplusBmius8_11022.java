ac4bcb062a9083fa5f916096691e1102bda3758e
Jogeahyun <whro815@naver.com>
2023-12-29 01:18:44 +0900

    AplusBmius8_11022.java (NoSuchElementException - 수정)

--- src/com/jgh/test/alone/loop/AplusBmius8_11022.java
+++ src/com/jgh/test/alone/loop/AplusBmius8_11022.java
@@ -1,24 +1,27 @@
 package com.jgh.test.alone.loop;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class AplusBmius8_11022 {
     public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer stringTokenizer;
+        StringTokenizer st;
 
-        int T = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(bf.readLine());
+        int a = 0;
+        int b = 0;
 
         for(int i = 0; i < T; i++){
-            stringTokenizer = new StringTokenizer(reader.readLine());
-            writer.write("Case #"+i+": "+Integer.parseInt(stringTokenizer.nextToken())+" + "+Integer.parseInt(stringTokenizer.nextToken())+" = "+(Integer.parseInt(stringTokenizer.nextToken())+Integer.parseInt(stringTokenizer.nextToken()))+"\n");
+            st = new StringTokenizer(bf.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+ (i+1) +": " + a + " + " + b +" = "+ (a+b)+"\n");
         }
+
+        bw.close();
 
     }
 }
