1c7350184b962e7d02334ebfd9e9235390bc2800
Jogeahyun <whro815@naver.com>
2024-02-04 22:58:00 +0900

    수 정렬하기(Arrays 메소드 + BufferReader + StringBuffer)

--- src/com/jgh/test/alone/sort/SortingNum_2750.java
+++ src/com/jgh/test/alone/sort/SortingNum_2750.java
@@ -4,15 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class SortingNum_2750 {
     public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner(System.in);
-//
-//        int total = sc.nextInt();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuffer sb = new StringBuffer();
 
         int total = Integer.parseInt(br.readLine());
         int[] arr = new int[total];
@@ -21,22 +19,14 @@ public class SortingNum_2750 {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-//        for (int i = 0; i < total-1; i++) {
-//            for (int j = i+1; j < total ; j++) {
-//                if(arr[i] > arr[j]){
-//                    int sub = arr[j];
-//                    arr[j] = arr[i];
-//                    arr[i] = sub;
-//                }
-//            }
-//        }
-
         //정렬 메소드
         Arrays.sort(arr);
 
-
         for (int i: arr) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+
+        System.out.println(sb);
+
     }
 }
