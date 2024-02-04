61fe26cf8de505268b00f3860a4035e054ecd17a
Jogeahyun <whro815@naver.com>
2024-02-04 22:43:07 +0900

    수 정렬하기(Scanner)

---
+++ src/com/jgh/test/alone/sort/SortingNum_2750.java
@@ -0,0 +1,33 @@
+package com.jgh.test.alone.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class SortingNum_2750 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+//
+        int total = sc.nextInt();
+          int []arr = new int[total];
+
+        for (int i = 0; i < total; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < total-1; i++) {
+            for (int j = i+1; j < total ; j++) {
+                if(arr[i] > arr[j]){
+                    int sub = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = sub;
+                }
+            }
+        }
+
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+}
