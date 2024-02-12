5f7eee43ef3aa2cb9d1b15c6ab8d978ec2c39581
Jogeahyun <whro815@naver.com>
2024-02-12 22:11:21 +0900

    다이얼 (BufferReader + switch case)

--- src/com/jgh/test/alone/stringEx/dial_5622.java
+++ src/com/jgh/test/alone/stringEx/dial_5622.java
@@ -1,12 +1,16 @@
 package com.jgh.test.alone.stringEx;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class dial_5622 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        String str = sc.next().toUpperCase();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toUpperCase();
 
         int count = 0;
 
