de2359c9351c06984f824a5403cdf5409b07f6c6
Jogeahyun <whro815@naver.com>
2024-02-14 00:36:34 +0900

    문자열 반복 문제(BufferReader + StringTokenizer)

--- src/com/jgh/test/alone/stringEx/strRepetition_2675.java
+++ src/com/jgh/test/alone/stringEx/strRepetition_2675.java
@@ -1,16 +1,23 @@
 package com.jgh.test.alone.stringEx;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class strRepetition_2675 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int total = sc.nextInt();
+        StringTokenizer st;
+
+        int total = Integer.parseInt(br.readLine());
         for (int i = 0; i < total; i++) {
 
-            int num = sc.nextInt();
-            String p = sc.next();
+            st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+            String p = st.nextToken();
 
             for (int j = 0; j < p.length(); j++) {
                 for (int k = 0; k < num; k++) {
