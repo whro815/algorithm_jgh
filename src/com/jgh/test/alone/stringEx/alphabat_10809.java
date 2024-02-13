bfa9a05eefc599fde35e4000ef489b6d12975e05
Jogeahyun <whro815@naver.com>
2024-02-13 19:47:23 +0900

    알파벳 문제(Scanner)

---
+++ src/com/jgh/test/alone/stringEx/alphabat_10809.java
@@ -0,0 +1,37 @@
+package com.jgh.test.alone.stringEx;
+
+import java.util.Scanner;
+
+public class alphabat_10809 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int[26];   // 알파벳 개수
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;    // 알파벳 전체 -1 세팅
+        }
+
+        String S = sc.next().toLowerCase();
+
+        for (int i = 0; i < S.length(); i++) {
+
+            // 초기화
+            char ch = S.charAt(i);
+
+            // arr 원소 값이 -1인 경우에만 초기화
+            if(arr[ch-'a'] == -1) {
+                arr[ch-'a'] = i;
+            }
+
+        }
+
+        for (int value: arr) {
+            System.out.print(value + " ");
+        }
+
+    }
+
+}
