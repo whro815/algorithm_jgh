4b41c6d5c814212ab55d1645f94a25ecfa591f94
Jogeahyun <whro815@naver.com>
2024-02-14 20:51:37 +0900

    팩토리얼2 문제(Scanner 2)

--- src/com/jgh/test/alone/recursive/Factorial2_27433.java
+++ src/com/jgh/test/alone/recursive/Factorial2_27433.java
@@ -6,23 +6,19 @@ public class Factorial2_27433 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        long res;
+        int n = sc.nextInt(); // 입력
+        long res = 1;         // 결과
 
-        if(n == 0){
-            res = 1;   // (같은) answer = 1;
-        } else { res = factorial(n); }
-
-        System.out.print(res);
-        sc.close();
-    }
-
-    static long factorial(long n) {
-        if(n > 1) { n = n * factorial(n-1); }
-        else {              // n 이 1일 경우
-            n = n * 1;      // n = 1 * 1;
+        for (int i = 1; i <= n; i++) {
+            if(n == 0){
+                res = 1;
+            } else {
+                res *= i;
+            }
         }
-        return n;
+
+        System.out.println(res);
+
     }
 
 }
