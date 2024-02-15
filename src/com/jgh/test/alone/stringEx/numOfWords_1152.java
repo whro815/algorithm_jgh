f0882baf1f2d5ee69d9dec834dfeb5229bb59e57
Jogeahyun <whro815@naver.com>
2024-02-15 23:45:54 +0900

    단어의 개수(BufferReader)

--- src/com/jgh/test/alone/stringEx/numOfWords_1152.java
+++ src/com/jgh/test/alone/stringEx/numOfWords_1152.java
@@ -1,16 +1,18 @@
 package com.jgh.test.alone.stringEx;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class numOfWords_1152 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        String s = sc.nextLine();
-        sc.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(s," ");
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        br.close();
 
         System.out.println(st.countTokens());
 
