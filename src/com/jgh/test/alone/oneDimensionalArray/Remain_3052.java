801f517986731b0ee8e22116d09db5d88bca9e51
Jogeahyun <whro815@naver.com>
2024-02-18 19:24:13 +0900

    나머지( BufferedReader + StringTokenzer + HashSet )

--- src/com/jgh/test/alone/oneDimensionalArray/Remain_3052.java
+++ src/com/jgh/test/alone/oneDimensionalArray/Remain_3052.java
@@ -1,18 +1,25 @@
 package com.jgh.test.alone.oneDimensionalArray;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Remain_3052 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> h = new HashSet<Integer>();
 
+        StringTokenizer st;
+
         for (int i = 0; i < 10; i++) {
-            h.add(sc.nextInt() % 42);
+            st = new StringTokenizer(br.readLine());
+            h.add(Integer.parseInt(st.nextToken()) % 42);
         }
 
-        sc.close();
+        br.close();
         System.out.println(h.size());
     }
 }
