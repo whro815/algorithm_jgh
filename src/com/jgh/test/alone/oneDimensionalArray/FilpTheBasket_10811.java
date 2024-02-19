1ff55d3bfd3297d63bab2986aed8444637333343
Jogeahyun <whro815@naver.com>
2024-02-19 12:05:15 +0900

    바구니 바꾸기( BufferedReader + StringTokenzer )

--- src/com/jgh/test/alone/oneDimensionalArray/FilpTheBasket_10811.java
+++ src/com/jgh/test/alone/oneDimensionalArray/FilpTheBasket_10811.java
@@ -1,13 +1,18 @@
 package com.jgh.test.alone.oneDimensionalArray;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class FilpTheBasket_10811 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();   // 5 바구니 총 N개
-        int M = sc.nextInt();   // 4 M번 바구니의 순서를 역순으로
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N =Integer.parseInt(st.nextToken());   // 5 바구니 총 N개
+        int M =Integer.parseInt(st.nextToken());   // 4 M번 바구니의 순서를 역순으로
 
         int arr[] = new int[N];
 
@@ -17,8 +22,10 @@ public class FilpTheBasket_10811 {
 
         for (int j = 0; j < M; j++) {
 
-            int L = sc.nextInt() -1;
-            int R = sc.nextInt() -1;
+            st = new StringTokenizer(br.readLine()," ");
+
+            int L = Integer.parseInt(st.nextToken()) - 1;
+            int R = Integer.parseInt(st.nextToken()) - 1;
 
             while (L < R){
                 int temp = arr[L];
@@ -28,7 +35,7 @@ public class FilpTheBasket_10811 {
 
         }
 
-        sc.close();
+        br.close();
 
         for (int basket: arr) {
             System.out.print(basket+" ");
