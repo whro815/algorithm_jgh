4b88b569bb48538e76112f4e1427569304ab026e
Jogeahyun <whro815@naver.com>
2024-03-10 20:04:47 +0900

    대표값2 (BufferedReader + Arrays)

--- src/com/jgh/test/alone/sort/RepresentativeValue_2587.java
+++ src/com/jgh/test/alone/sort/RepresentativeValue_2587.java
@@ -1,11 +1,13 @@
 package com.jgh.test.alone.sort;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class RepresentativeValue_2587 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int arr[] = new int[5];
 
@@ -13,7 +15,7 @@ public class RepresentativeValue_2587 {
         int avg = 0;    // 평균 값
 
         for (int i = 0; i < 5; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
