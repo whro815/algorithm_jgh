bf4f5202b8539c0cc529de9967036c58d3a5c158
Jogeahyun <whro815@naver.com>
2024-03-13 23:17:43 +0900

    수 정렬하기 3 (BufferedReader + StringBuilder + Arrays)

---
+++ src/com/jgh/test/alone/sort/SortingNum3_10989.java
@@ -0,0 +1,32 @@
+package com.jgh.test.alone.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class SortingNum3_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i: arr) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
